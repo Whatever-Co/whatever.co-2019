99b64f5fbe29b0774b55647a86a43a6a9aea087c
Saqoosha <a@saqoo.sh>
2019-01-23 12:50:30 +0900

    swap..

--- src/components/Statement.jsx
+++ src/components/Statement.jsx
@@ -10,8 +10,8 @@ module.exports = React.createClass({
 
     data: {
         en: {
-            text_pc: 'Creative agency PARTY New York and PARTY Taipei, <br />are merging with Japan’s renowned<br />digital production company dot by dot<br />to start a new global studio “Whatever”.',
-            text_sp: 'Creative agency PARTY New York and PARTY Taipei, <br />are merging with Japan’s renowned digital production company dot by dot<br />to start a new global studio “Whatever”.',
+            text_pc: 'Creative agency PARTY New York and PARTY Taipei, <br />are merging with Japan’s renowned digital production company dot by dot<br />to start a new global studio “Whatever”.',
+            text_sp: 'Creative agency PARTY New York and PARTY Taipei, <br />are merging with Japan’s renowned<br />digital production company dot by dot<br />to start a new global studio “Whatever”.',
             image_pc: '/assets/statement-en.svg',
             image_sp: '/assets/statement-en-sp.svg',
             alt: '“PARTY (New York & Taipei)” is a creative agency that loves to make weird and wonderful things with their own hands. “dot by dot” is a digital production company that loves to create new ideas and strategies using their own brains.  Descriptions aside, the two teams share one similarity: Before anything else, we are creatives who “think and make”, because we believe that\'s the only way to bring dreamlike ideas to life.Many agencies and production companies around the world share the same mission of "thinking and making", but only few have walked the talk.Our challenge is to prove to the world that we can make whatever we imagine through our Tokyo, New York, Berlin, and Taipei teams.',
@@ -19,8 +19,8 @@ module.exports = React.createClass({
             names: ['Yusuke Tominaga', 'Masashi Kawamura', 'Saqoosha']
         },
         ja: {
-            text_pc: 'dot by dot 、PARTY New York 、PARTY Taipei は<br />このたび合併し、1つの新しい組織として<br />「Whatever」をスタートします',
-            text_sp: 'dot by dot 、PARTY New York 、PARTY Taipei はこのたび合併し、<br />1つの新しい組織として「Whatever」をスタートします',
+            text_pc: 'dot by dot 、PARTY New York 、PARTY Taipei はこのたび合併し、<br />1つの新しい組織として「Whatever」をスタートします',
+            text_sp: 'dot by dot 、PARTY New York 、PARTY Taipei は<br />このたび合併し、1つの新しい組織として<br />「Whatever」をスタートします',
             image_pc: '/assets/statement-ja.svg',
             image_sp: '/assets/statement-ja-sp.svg',
             alt: '自ら手を動かして作りたいクリエイティブエージェンシー“PARTY”と、自分たちの頭で考えたいプロダクション“dot by dot”。わたしたちに共通して言えるのは「考えて、作りたい」人の集まりだということ。なぜならそれこそが、夢のようなアイデアを夢のような形で実現できる唯一のつくり方だからです。これを標榜しているエージェンシーやプロダクションはたくさん出現していますが、残念ながらこれを本当に実現できているチームは、世界を見渡してもまだほとんど存在しません。わたしたちは東京、ニューヨーク、台北、ベルリンを拠点として、日本のみならず世界をもフィールドと定め、「考えて作る」ことの力を実証していきたいと思っています。',
