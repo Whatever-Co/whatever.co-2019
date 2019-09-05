9c6d8adf64c51088de78988ee0d0253ddadf2eba
Saqoosha <a@saqoo.sh>
2019-09-05 12:47:12 +0900

    JA SP

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,15 +11,18 @@ module.exports = React.createClass({
 
     data: {
         en: {
-            poem_pc: '/assets/pome-ja.svg',
+            poem_pc: '/assets/poem-ja.svg',
+            poem_sp: '/assets/poem-ja-sp.svg',
             alt: 'Whatever is a creative studio that aims to conceive unexplored ideas, and bring them to life by whatever means possible. Based in Tokyo, New York, Taipei, and Berlin, our multidisciplinary team allows us to produce innovative work in various creative fields such as advertising and brand design, to content productions such as TV  shows, products, and service designs. By “thinking and making” under one roof, we strive to create beautiful experiences that can be talked and endeared by the people around the world.',
         },
         ja: {
-            poem_pc: '/assets/pome-ja.svg',
+            poem_pc: '/assets/poem-ja.svg',
+            poem_sp: '/assets/poem-ja-sp.svg',
             alt: 'わたしたちは、誰も見たことがないようなアイデアを考え、あらゆる方法でそれを実現していくクリエイティブスタジオです。東京、ニューヨーク、台北、ベルリンの拠点を活かし、世界を舞台に広告やブランディングから、テレビ番組、ゲーム、プロダクトなどのコンテンツや新規事業開発まで、様々な分野のモノづくりを手がけていきます。考えるだけでもない、作るだけでもない、「考えて作る」ことで生まれるアイデアと実現力を武器に、世界中の人に驚かれ愛されるような体験を作り上げていきます。',
         },
         zh: {
-            poem_pc: '/assets/pome-ja.svg',
+            poem_pc: '/assets/poem-ja.svg',
+            poem_sp: '/assets/poem-ja-sp.svg',
             alt: '​​Whatever 是一家擅長發想出前所未有的創意，並透過各式各樣的方式將其化為現實的創意工作室。我們充分活用分布於東京、紐約、台北及柏林的據點，以全世界為舞台，提供從廣告、體驗行銷、品牌策略、到內容創作及產品開發等的多領域創意製作服務。我們不僅限於只發想創意，或單純提供製作，而是兼具「動腦想和動手做」的優勢，來創造出讓全世界驚豔且著迷的體驗。',
         },
     },
@@ -30,8 +33,8 @@ module.exports = React.createClass({
 
     render() {
         return <div className="about">
-            <img className="about-text" src="/assets/03_about_03.svg" alt="Make whatever. Rules, whatever." />
-            <img className="about-poem" src={this.state.poem_pc} alt={this.state.alt} />
+            <img className="about-text" src={isMobile ? "/assets/makewhatever-sp.svg" : "/assets/makewhatever.svg"} alt="Make whatever. Rules, whatever." />
+            <img className="about-poem" src={isMobile ? this.state.poem_sp : this.state.poem_pc} alt={this.state.alt} />
             <div className="about-chiefs">
                 <Link className="about-chief" to="/team/yusuke/">
                     <img src="https://whatever.co/wp-content/uploads/2018/01/dotbydot7168.jpg"/>
