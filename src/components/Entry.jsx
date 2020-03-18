7ab1dbe853d6e10d9df6e5d657322ba41afa65d9
Saqoosha <a@saqoo.sh>
2020-03-18 17:53:18 +0900

    Add link to entry title

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -185,7 +185,7 @@ module.exports = React.createClass({
         return (
             <div className={cx({ entry: true, 'with-image': entry.featured_image })} ref="entry" style={style}>
                 <div className={cx({ inner: true, hover: this.props.single })} ref="inner">
-                    <h1 className="title" dangerouslySetInnerHTML={{ __html: entry.title }} />
+                    <h1 className="title"><Link to={`/post/${entry.slug}/`}><span dangerouslySetInnerHTML={{ __html: entry.title }} /></Link></h1>
                     <span className="date"><Link to={`/post/${entry.slug}/`}>{moment(entry.date_gmt).format('LL')}</Link></span>
                     <div className="body" ref="body">
                         <div dangerouslySetInnerHTML={{ __html: this._replaceJSONLink(entry.content) }} />
